51eddc149bfcdf7105eced8b3324a2016f6e40b8
BasilAboSaleem <basilsaleem11@gmail.com>
2025-12-19 17:34:23 +0200

    chore: add app and server setup, update npm scripts, and run server successfully

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const app = require('./app');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Gaza E Meter running on http://localhost:${PORT}`);
+});
