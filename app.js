536750339903c214dbfacf4ffdaff2ad388ce854
BasilAboSaleem <basilsaleem11@gmail.com>
2025-12-20 10:09:00 +0200

    Refactor dashboard index by extracting reusable partials

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const path = require('path');
 
 const app = express();
 
+// Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// View engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-
 
 module.exports = app;
